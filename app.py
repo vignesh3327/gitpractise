cb4a34e2e0ba3ec6b4a40d748373568bdf25a568
vignesh3327 <vigneshrock9090@gmail.com>
2026-09-03 14:09:51 +0530

    created app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from kubernetes import client, config
+
+# Load in-cluster config
+config.load_incluster_config()
+
+# Create API client
+v1 = client.CoreV1Api()
+
+# List pods in current namespace
+pods = v1.list_namespaced_pod(namespace="default")
+
+for pod in pods.items:
+    # (The rest of the loop is cut off in the video, but typically you would print the pod name here, e.g.:)
+    print(pod.metadata.name)
